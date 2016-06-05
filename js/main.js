db18648dd4bb6ca55313f22f6046911e9d27358b
kevinjsheen <kevinjsheen@gmail.com>
2016-06-04 22:34:08 -0400

    Added social icons and map to page

--- js/main.js
+++ js/main.js
@@ -26,4 +26,42 @@ $('.view-details').on('click', function(event){
 	   });
 	});
 	
+	var map = L.map( 'map', {
+    center: [47.6135934, -122.3443024],
+    minZoom: 5,
+    zoom: 13
+	});
+	
+	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+	var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 18, attribution: osmAttrib});
+
+	var satLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png', {
+    	attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+    	subdomains: ['otile1','otile2','otile3','otile4']
+	});
+
+	var drawLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
+    	attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+    	subdomains: ['otile1','otile2','otile3','otile4']
+});
+
+	var mapLayers = {
+    	"Map View": drawLayer,
+		"Satellite": satLayer,
+    	"Open Street Maps": osm
+};
+	
+	L.control.layers(mapLayers).addTo(map);
+	satLayer.addTo(map);
+	
+	var marker = L.marker([47.6135934, -122.3443024]).addTo(map);
+	marker.bindPopup("<b>Welcome to The Crocodile!</b><br>Est. 1991.");
+	
+	var marker = L.marker([47.6085655, -122.3393417]).addTo(map);
+	marker.bindPopup("<b>Welcome to The Showbox Market!</b><br>Est. 1939.");
+	
+	var marker = L.marker([47.6132851, -122.3313795]).addTo(map);
+	marker.bindPopup("<b>Welcome to The Paramount Theatre!</b><br>Est. 1928.");
+	
 }); 
